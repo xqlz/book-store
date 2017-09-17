2205214c5a30cf76164ecc29624a85f2fd90b488
xqlz <zefangelica@gmail.com>
2017-09-17 17:32:28 +0800

    add, show, new, create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,26 @@ class BooksController < ApplicationController
 	def index
 		@books = Book.all
 	end
+
+	def show
+		@book = Book.find(params[:id]) # parameters (in routes.rb); from route matching, query strings
+	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		book = Book.new(book_params) # sanitized the params
+
+		if book.save
+			redirect_to books_path
+		else	
+			render :new # generate new form
+		end
+	end
+
+	private def book_params # sanitizing params - output only the things we need
+		params.require(:book).permit(:name, :author, :price, :quantity)
+	end
 end

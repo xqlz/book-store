48b7fb33ce2d9a1ab3fe1802bc67a2ccff12c72b
xqlz <zefangelica@gmail.com>
2017-09-25 11:24:40 +0800

    ilhan's tutelage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,25 @@ class BooksController < ApplicationController
 		end
 	end
 
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		book = Book.find(params[:id])
+
+		if book.update(book_params)
+			redirect_to books_path
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		Book.find(params[:id]).destroy
+		redirect_to books_path
+	end
+
 	private def book_params # sanitizing params - output only the things we need
 		params.require(:book).permit(:name, :author, :price, :quantity)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/books', to: 'books#index' #"get" = http verb; controller: books, action: index
-  get '/book/:id', to: 'books#show'
-  get '/books/new', to: 'books#new'
-  post '/books', to: 'books#create'
+  resources :books #"get" = http verb; controller: books, action: index
 end
